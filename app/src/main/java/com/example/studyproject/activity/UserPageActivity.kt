5e747222511b2fc6ced6a89451c5c2769c9ce8fc
acaewd <acaewd@gmail.com>
2021-07-19 15:24:56 +0900

    First Update

---
+++ app/src/main/java/com/example/studyproject/activity/UserPageActivity.kt
@@ -0,0 +1,32 @@
+package com.example.studyproject.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.studyproject.R
+
+class UserPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_page)
+
+        val Id = intent.getStringExtra("Id").toString()
+        val Pw = intent.getStringExtra("Pw").toString()
+
+        var checkButton = findViewById<Button>(R.id.btnCheck)
+
+        var nextButton = findViewById<Button>(R.id.btnNext)
+
+        checkButton.setOnClickListener(){
+            Log.d("아이디:", Id)
+            Log.d( "비밀번호:", Pw)
+        }
+
+        nextButton.setOnClickListener(){
+            val intent = Intent(this,ScrollViewActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
